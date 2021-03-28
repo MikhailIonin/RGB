0a601da1042525d3b66486422af66f812ed4d909
Mikhail Ionin <mikhail.ionin@gmail.com>
2021-03-28 12:46:13 +0700

    repetitions no constraints

--- RGB/ViewController.swift
+++ RGB/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSliderLabel: UILabel!
+    @IBOutlet var greenSliderLabel: UILabel!
+    @IBOutlet var blueSliderLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = colorView.frame.width / 20
+    
+        colorView.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
 
-
+    @IBAction func redSliderAction() {
+        redSliderLabel.text = NSString(format: "%.2f", redSlider.value) as String
+        
+        colorView.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenSliderLabel.text = NSString(format: "%.2f", greenSlider.value) as String
+        
+        colorView.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueSliderLabel.text = NSString(format: "%.2f", blueSlider.value) as String
+        
+        colorView.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
 }
 
